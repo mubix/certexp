51617eff405fc7c72dbbd5a3352c2bc81aa8715b
Rob Fuller <jd.mubix@gmail.com>
2019-09-22 17:55:14 -0400

    first commit

---
+++ certexp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.IO;
+
+namespace certexp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randompath = Path.GetRandomFileName();
+            string pass = randompath.Replace(".", "");
+            System.Security.Cryptography.X509Certificates.X509Store store1 = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            store1.Open(OpenFlags.ReadOnly);
+            Console.WriteLine("===== System Certificates ====");
+            foreach (X509Certificate2 certificate in store1.Certificates)
+            {
+                Console.WriteLine("[+] Subject Name: " + certificate.Subject);
+                Console.WriteLine("[+] Thumbprint  : " + certificate.Thumbprint);
+                try
+                {
+                    if (certificate.PrivateKey != null)
+                    {
+                        Console.WriteLine("[+] Private Key : YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("[+] Private Key : NO");
+                    }
+                    byte[] certBytes = certificate.Export(X509ContentType.Pkcs12, pass);
+                    Console.WriteLine(System.Convert.ToBase64String(certBytes));
+                    Console.WriteLine("\n");
+                }
+                catch
+                {
+                    Console.WriteLine("----> COULD NOT EXPORT - NOT ADMIN OR NOT MARKED AS EXPORTABLE");
+                }
+            }
+            System.Security.Cryptography.X509Certificates.X509Store store2 = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store2.Open(OpenFlags.ReadOnly);
+            Console.WriteLine("\n\n===== User Certificates ======");
+            foreach (X509Certificate2 certificate in store2.Certificates)
+            {
+                Console.WriteLine("[+] Subject Name: " + certificate.Subject);
+                Console.WriteLine("[+] Thumbprint  : " + certificate.Thumbprint);
+                try
+                {
+                    if (certificate.PrivateKey != null)
+                    {
+                        Console.WriteLine("[+] Private Key : YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("[+] Private Key : NO");
+                    }
+                    byte[] certBytes = certificate.Export(X509ContentType.Pkcs12, pass);
+                    Console.WriteLine(System.Convert.ToBase64String(certBytes));
+                    Console.WriteLine("\n");
+                }
+                catch
+                {
+                    Console.WriteLine("----> COULD NOT EXPORT - NOT ADMIN OR NOT MARKED AS EXPORTABLE");
+                }
+            }
+            Console.WriteLine($"[*] Export password is: {pass}");
+        }
+    }
+}
